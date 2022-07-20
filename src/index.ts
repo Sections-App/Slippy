a52a5d36f4685165065f68845fb3483a2fa83fba
Roman Lytvynenko <litvinenko95@gmail.com>
2022-07-20 10:26:13 +0300

    rename fns fix

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export { useDrag } from './droppable/useDrag'
 export { useDrop } from './droppable/useDrop'
 
-export { useSortableItem } from './sortable/useDragItem'
-export { useSortableContainer } from './sortable/useDropArea'
+export { useSortableItem } from './sortable/useSortableItem'
+export { useSortableContainer } from './sortable/useSortableContainer'
 export { arrayMove, arrayRemove, insert } from './sortable/utils'
 
-export type { DropRect } from './sortable/useDropArea'
+export type { DropRect } from './sortable/useSortableContainer'
